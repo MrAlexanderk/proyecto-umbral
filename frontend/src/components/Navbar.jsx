a0b21b0009108d023b9fabd9d95f75f29182bbfb
Tom <70593510+MrAlexanderk@users.noreply.github.com>
2025-08-05 22:28:54 -0400

    Navbar added;

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import '../styles/Navbar.css';
+import logo from '../assets/logo.png';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-custom sticky-top">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand logo d-flex align-items-center text-white-custom text-spectral" to="/">
+          <img src={logo} alt="El Umbral Logo" className="navbar-logo" />
+          El Umbral
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav ms-auto gap-3 text-white-custom text-spectral">
+            <NavLink className="nav-link" to="/" end>Home</NavLink>
+            <NavLink className="nav-link" to="/artifacts">Artifacts</NavLink>
+            <NavLink className="nav-link" to="/collections">Collections</NavLink>
+            <NavLink className="nav-link" to="/cart">
+              <FaShoppingCart size={18} />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
